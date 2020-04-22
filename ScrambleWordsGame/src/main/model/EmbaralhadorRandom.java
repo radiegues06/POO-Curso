a63e7a4ecd489f8e058f4c6855a9d3bfc887382a
rafaelamanciodiegues@gmail.com <ri65%ma58R>
2020-04-22 16:02:27 -0300

    Classe EmbaralhadorRandom

---
+++ ScrambleWordsGame/src/main/model/EmbaralhadorRandom.java
@@ -0,0 +1,54 @@
+package main.model;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class EmbaralhadorRandom implements Embaralhador {
+
+    private Random random = new Random();
+    private String originalWord = new String();
+
+
+    @Override
+    public String getScrambleWord(String word) {
+
+        if (word==null || word.isEmpty())
+            return "";
+
+        this.originalWord = word;
+
+        return scrambleWord();
+    }
+
+    private String scrambleWord() {
+
+        String shuffledString = new String();
+
+        for (Integer i:  generateShuffleIndexes(originalWord.length())) {
+            shuffledString += originalWord.charAt(i);
+        }
+
+        return shuffledString;
+    }
+
+    @Override
+    public String getScrambleMode() {
+        return "Troca letras aleatoriamente.";
+    }
+
+    private List<Integer> generateShuffleIndexes(int length) {
+
+        List<Integer> indexes = new ArrayList<>();
+
+        for (int i = 0; i < length; i++) {
+            indexes.add(i);
+        }
+
+        Collections.shuffle(indexes);
+
+        return indexes;
+    }
+}
